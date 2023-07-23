2426ba71ea776403e8c390ce1831a80d18e95472
Pangsaang Lee <462889098@qq.com>
2023-07-23 19:20:35 +0800

    create gameplay framework

---
+++ Source/CubeGame/CubeGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CubeGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBEGAME_API UCubeGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CubeGame/CubePlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CubePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBEGAME_API ACubePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
